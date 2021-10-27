38bc81fe31628dd9c6e9af16514c0bd6b3601366
stajun2 <stajun@hanmail.net>
2021-10-27 16:51:07 +0900

    1-5 : 문자열 선언 예제

---
+++ jik/src/day1/StringEx1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		/* 문자열 String 
+		 * */
+		String str1;
+		String str2 = "Hello";
+		System.out.println(str2);
+		str1 = str2 + " world!";
+		System.out.println(str1);
+		
+	}
+}
