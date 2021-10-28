065dd2d3966820dd40f0d3af610b468051c64d2e
stajun2 <stajun@hanmail.net>
2021-10-28 17:39:29 +0900

    2-11 : if문 홀짝 예제

---
+++ jik/src/day2/IfEvenEx1.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class IfEvenEx1 {
+
+	public static void main(String[] args) {
+		/* num가 짝수이면 짝수라고 출력하는 코드를 작성하세요.
+		 * 조건식 : num가 짝수이다
+		 *        num가 2의 배수이다
+		 *        num를 2로 나누었을 때 나머지가 0이다
+		 *        num를 2로 나누었을 때 나머지가 0과 같다
+		 * 실행문 : 짝수라고 출력
+		 * */
+		int num = 10;
+		if(num % 2 == 0) {
+			System.out.println(num + "는 짝수");
+		}
+		if(num % 2 != 0) {
+			System.out.println(num + "는 홀수");
+		}
+	}
+}
