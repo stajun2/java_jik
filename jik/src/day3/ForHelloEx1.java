c10b07f1c9af2fa106f6019e2b4c9e7662e35ecf
stajun2 <stajun@hanmail.net>
2021-10-29 17:00:40 +0900

    3-9 : for문을 이용한 Hello 출력 예제

---
+++ jik/src/day3/ForHelloEx1.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class ForHelloEx1 {
+
+	public static void main(String[] args) {
+		/* Hello를 5번 출력하는 코드를 작성하세요. 
+		 * 규칙성 : Hello를 출력
+		 * 반복횟수 : 5번, 숫자는 중요하지 않음.
+		 * 출력 결과
+		 * Hello 	i = 1
+		 * Hello	i = 2
+		 * Hello	i = 3
+		 * Hello	i = 4
+		 * Hello	i = 5
+		 * */
+		int i;
+		for(i = 1 ; i <= 5 ; i++ ) {
+			System.out.println("Hello");	
+		}
+	}
+}
