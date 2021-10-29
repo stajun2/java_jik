93466b6188f1efe188de4d95a0e611569c920a31
stajun2 <stajun@hanmail.net>
2021-10-29 17:01:13 +0900

    3-10 : for문을 이용한 연속된 숫자 출력 예제

---
+++ jik/src/day3/ForNumberEx1.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class ForNumberEx1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 for문으로 작성하세요.
+		 * 반복횟수 : 5번. 1부터 5까지(같다를 포함) 1씩 증가
+		 * 출력 예시
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * */
+		int i;
+		for( i = 1 ; i <= 5 ; i++ ) {
+			System.out.println(i);
+		}
+	}
+
+}
