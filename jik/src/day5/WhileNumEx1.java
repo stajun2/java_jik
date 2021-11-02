f14e577985391a590350ca03e1a5a1e5acac462f
stajun2 <stajun@hanmail.net>
2021-11-02 16:19:37 +0900

    5-2 : while문 연속 숫자 출력 예제

---
+++ jik/src/day5/WhileNumEx1.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class WhileNumEx1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 while문으로 작성하세요.
+		 * */
+		int i;
+		i = 1;
+		while( i<=5 ) {
+			System.out.print(i + " ");
+			i++;
+		}
+	}
+
+}
