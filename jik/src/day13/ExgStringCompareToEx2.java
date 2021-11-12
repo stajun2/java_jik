5839dee838d39d8bd569114f2967860af9a323e0
stajun2 <stajun@hanmail.net>
2021-11-12 15:43:32 +0900

    13-8 : 문자열 compareTo() 예제

---
+++ jik/src/day13/ExgStringCompareToEx2.java
@@ -0,0 +1,14 @@
+package day13;
+
+public class ExgStringCompareToEx2 {
+
+	public static void main(String[] args) {
+		/* 문자열 4개를 한곳에서 관리할 수 있게 배열을 만드세요. */
+
+		/* 배열에 b d a c 순으로 저장하세요. */
+		
+		/* 반복문과 compareTo를 이용하여 사전순으로 정렬해보세요. */
+		
+	}
+
+}
