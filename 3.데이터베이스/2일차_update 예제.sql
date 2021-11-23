5f50b1856d480844c1e5750a32aa8d2f77182b5a
stajun2 <stajun@hanmail.net>
2021-11-23 17:19:31 +0900

    DB 2일차 : update 예제

--- 3.데이터베이스/2일차_update 예제.sql
+++ 3.데이터베이스/2일차_update 예제.sql
@@ -4,11 +4,14 @@ update student
 		st_id = 'kangkidong', 
         st_pw = 'kangkidongpw' 
 	where 
-		st_num = '2021123001';
+		st_num = 2021123001;
 update student 
 	set 
 		st_id = 'kangkidong', 
         st_pw = 'kangkidongpw' 
 	where 
 		st_name = '유관순';
-SELECT * FROM university_jik.student;
+        
+-- 2000160001교수님이 1번 과목을 강의할 강의 시간표의 일정이 나왔다. 월1,2로 결정되었고, 분반은 3분반이다.
+-- 다음을 실행하는 쿼리문을 작성하세요.
+update subject set su_schedule = '월1, 월2', su_class_num = 3 where su_num = 1;
