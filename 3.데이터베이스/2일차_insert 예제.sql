f0f7f70f41c844893579662f056d92e06dcf737d
stajun2 <stajun@hanmail.net>
2021-11-23 17:19:52 +0900

    DB 2일차 : insert 예제

--- 3.데이터베이스/2일차_insert 예제.sql
+++ 3.데이터베이스/2일차_insert 예제.sql
@@ -7,7 +7,21 @@
 -- id : kimid, pw : pwkim으로 하기 위해 속성 순서를 st_pw, st_id 순으로 바꿔서 원하는 결과가 나옴
 -- insert into student(st_num, st_pw, st_id,  st_name, st_reg_num)
 -- 	values('2021160015','pwkim','kimid','김영희','021225-4654321');
-insert into student 
-	values(2021123005, 'park', 'parkpw', '박철수','021221-4443332'),
-	(2021456005, 'parkid', 'parkpw', '박수철','020808-3985612');
-SELECT * FROM university_jik.student;
+-- insert into student 
+-- 	values(2021123005, 'park', 'parkpw', '박철수','021221-4443332'),
+-- 	(2021456005, 'parkid', 'parkpw', '박수철','020808-3985612');
+
+-- 교수번호가 2000160001인 이순신 교수님의 정보를 추가하는 쿼리문을 작성하세요.
+-- id : lee, pw : leelee, 주민번호 : 550505-1234567
+-- insert into professor values(2000160001, 'lee', 'leelee', '이순신', '550505-1234567');
+
+-- 과목명이 컴퓨터 개론이고, 과목 코드는 MSC001, 일정과 분반은 미정인 과목을 추가하는 쿼리문을 작성하세요.
+-- insert into subject(su_code, su_title) values('MSC001', '컴퓨터 개론');
+
+-- 2000160001교수님이 1번 과목을 강의한다. 이 내용을 lecture 테이블에 추가하는 코드를 작성하세요.
+-- insert into lecture(le_pr_num, le_su_num) values(2000160001, 1);
+
+-- 2021160123 홍길동 학생이 1번 과목을 수강신청 했다. 이 내용을 추가하는 코드를 작성하세요.
+-- insert into course(co_st_num, co_su_num) values(2021160123, 1);
+
+
