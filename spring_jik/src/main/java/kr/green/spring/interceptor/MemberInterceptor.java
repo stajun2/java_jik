d8e12d493c311d74ab60e1e3a5b4e959665e0271
stajun2 <stajun@hanmail.net>
2022-01-10 15:19:18 +0900

    스프링 : Interceptor를 이용한 페이지 접근 권한 설정

---
+++ spring_jik/src/main/java/kr/green/spring/interceptor/MemberInterceptor.java
@@ -0,0 +1,23 @@
+package kr.green.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MemberInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+		//세션에 있는 회원정보를 가져옴. 세션에 user로 저장했기에 user를 가져옴
+		Object user = request.getSession().getAttribute("user");
+		//세션에 회원정보가 없으면 => 로그인을 안했으면
+		if(user == null) {
+			response.sendRedirect(request.getContextPath()+"/login");
+			return false;
+		}
+		return true;
+	}
+}
