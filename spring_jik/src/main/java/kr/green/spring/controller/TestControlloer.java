4a73f4de40ec32a40a09dd31a0cfa564f1533072
stajun2 <stajun@hanmail.net>
2022-02-14 14:19:26 +0900

    샘플 : 컨트롤러에서 화면으로 alert 보내는 예제

---
+++ spring_jik/src/main/java/kr/green/spring/controller/TestControlloer.java
@@ -0,0 +1,28 @@
+package kr.green.spring.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestControlloer {
+	@RequestMapping("/sample/test1")
+	public ModelAndView adminMemberList(ModelAndView mv, 
+			HttpServletResponse response, Integer test) throws IOException {
+		if(test != null) {
+			response.setContentType("text/html; charset=utf-8");
+			response.setCharacterEncoding("utf-8");
+			PrintWriter out = response.getWriter();
+      out.println("<script>alert('알림');</script> ");
+      out.flush();
+		}
+		mv.setViewName("/main/home");
+		
+		return mv;
+	}
+}
