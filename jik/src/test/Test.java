2bfe42e80b565c4a5eaad18a202b514fc8a7cae3
stajun2 <stajun@hanmail.net>
2022-04-07 15:30:59 +0900

    공공데이터르 이용한 예제 : 수정

--- jik/src/test/Test.java
+++ jik/src/test/Test.java
@@ -14,9 +14,7 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList"); /*URL*/
     urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=yokZLTATxLSYExFfHZfEWjWDoB6g9Q0fIcgD3rXjIO0bvc7AB5H5QIDdggjBlOqvGMj7ouM7SWGAU24hcvDOEQ%3D%3D"); /*Service Key*/
-    //페이지정보. 테스트는 1페이지로함.
     urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-    //한페이지에서 보여지는 결과물 갯수
     urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
     //구분코드 : A0: 국민안심병원/97: 코로나검사 실시기관/99: 코로나 선별진료소 운영기관
     urlBuilder.append("&" + URLEncoder.encode("spclAdmTyCd","UTF-8") + "=" + URLEncoder.encode("A0", "UTF-8")); /*A0: 국민안심병원/97: 코로나검사 실시기관/99: 코로나 선별진료소 운영기관*/
@@ -38,16 +36,14 @@ public class Test {
 				
 				Element eElement = (Element) nNode;
 				System.out.println("######################");
-				//System.out.println(eElement.getTextContent());
 				System.out.println("운영 가능 일자  : " + getTagValue("adtFrDd", eElement));
-				System.out.println("선정 유형  : " + getTagValue("hospTyTpCd", eElement));
 				System.out.println("시군구명 : " + getTagValue("sgguNm", eElement));
 				System.out.println("시도명  : " + getTagValue("sidoNm", eElement));
 				System.out.println("구분코드  : " + getTagValue("spclAdmTyCd", eElement));
 				System.out.println("전화번호  : " + getTagValue("telno", eElement));
 				System.out.println("기관명  : " + getTagValue("yadmNm", eElement));
-			}	// for end
-		}	// if end
+			}
+		}
 		
 	}
 	private static String getTagValue(String tag, Element eElement) {
